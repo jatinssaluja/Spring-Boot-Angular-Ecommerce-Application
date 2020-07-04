409493316be6b561cf1f89fc91c1c29dc0f2426b
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-07-04 13:57:55 +0200

    Add CORS support to springboot backend

--- backend/spring-boot-ecommerce/src/main/java/com/jatin/ecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/jatin/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.jatin.ecommerce.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jatin.ecommerce.entity.ProductCategory;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 // collectionResourceRel represents the name of JSON entry
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
